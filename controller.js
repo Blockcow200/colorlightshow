e50b77b4cc0dc66bcea2e50749a99d47f5a71900
Blockcow200 <raphaeltse200@gmail.com>
2025-02-17 08:51:46 +0800

    Delete controller.js

--- controller.js
+++
@@ -1,8 +0,0 @@
-const socket = new WebSocket('wss://colour-project.glitch.me/');
-const colorPicker = document.getElementById('colorPicker');
-
-// Send the selected color to the server
-colorPicker.addEventListener('input', (event) => {
-  const color = event.target.value;
-  socket.send(color);
-});
